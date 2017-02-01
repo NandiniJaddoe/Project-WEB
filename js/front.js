72fb63103fc3984077d092f0c144ebaf6a5aa439
Nandini Jaddoe <n.d.jaddoe@outlook.com>
2017-02-01 17:36:58 +0100

    updates

--- js/front.js
+++ js/front.js
@@ -1,37 +1,55 @@
 (function() {
-
-	// Variabelen voor de carousel
 	var carousel = {}; // Maakt een leeg object
-	var links = document.getElementsByClassName('header-img a'); // Maakt een variabele van alle links
-	var headerImg = document.getElementsByClassName('header-img img'); // Maakt een variabele van alle opgegeven images
+	var links = document.querySelectorAll('.header-img a'); // Maakt een variabele van alle links
+	var headerImg = document.querySelectorAll('.header-img img'); // Maakt een variabele van alle opgegeven images
 
 	// Zegt wat bij welke varibele hoort 
-	carousel.links = $links; // Voegt object toe met data van het links var
-	carousel.images = $headerImg; // Voegt object toe met data van het images var
+	carousel.links = links; // Voegt object toe met data van het links var
+	carousel.images = headerImg; // Voegt object toe met data van het images var
 
 	// Voegt image object toe aan link object
-	carousel.links.each(function(obj) {
-		carousel.links[obj].image = carousel.images[obj];
+	carousel.links.forEach(function(obj, index) {
+		carousel.links[index].image = carousel.images[index];
 	});
 
-	// Zorgt ervoor dat wanner je op een lik klikt de carouselClasses functie wordt uitgevoerd
-	$links.on('click', carouselClasses);
+	// Zorgt ervoor dat wanner je op een link klikt de carouselClasses functie wordt uitgevoerd
+	carousel.links.forEach(function(obj) {
+		obj.addEventListener('click', clickOnLink);
+	});
 
-	function carouselClasses() {
-		var $clicked = $(this)[0]; // clicked is het zelfde als de link
+	function clickOnLink() {
+		var className = 'active';
 
-		$(this).addClass('active'); // Zegt dat de link active is
-		carousel.links.each(function(obj) { 
-			var $image = $(carousel.links[obj].image);
-			if ($clicked.innerHTML == carousel.links[obj].innerHTML) {
-				if ($headerImg.hasClass('active')) {
-					$headerImg.removeClass('active');
-				} 
-				$image.addClass('active');
-			} else {
-				// do nothing
-			}
+		links.forEach(function(obj) {
+			if (obj.classList)
+			  obj.classList.remove(className);
+			else
+			  obj.className = obj.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
 		});
+		if (this.classList) {
+			this.classList.add(className);
+		}
+		else {
+			this.className += ' ' + className;
+		}
+		turnCarousel(this.image);
+	}
+
+	function turnCarousel(image) {
+		var className = 'active';
+
+		headerImg.forEach(function(obj) {
+			if (obj.classList)
+			  obj.classList.remove(className);
+			else
+			  obj.className = obj.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+		});
+		if (image.classList) {
+			image.classList.add(className);
+		}
+		else {
+			image.className += ' ' + className;
+		}
 	}
 
 })();
